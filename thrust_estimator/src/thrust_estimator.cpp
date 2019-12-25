11296924df38f7e26fc29caacd9013ad03e13caa
Christian Llanes <christianllanes96@gmail.com>
2019-12-24 22:02:07 -0500

    update

--- thrust_estimator/src/thrust_estimator.cpp
+++ thrust_estimator/src/thrust_estimator.cpp
@@ -41,9 +41,10 @@ int main(int argc, char** argv)
   std::cout << ADC.sendSystemCommand(ADS131A04_ADC::CMD_UNLOCK) << std::endl;
 
 
-//  std::cout << ADC.writeRegister(ADS131A04_ADC::ADDR_CLK1,0x02) << std::endl;
+  std::cout << ADC.writeRegister(ADS131A04_ADC::ADDR_CLK1,0x02) << std::endl;
 
   std::cout << ADC.writeRegister(ADS131A04_ADC::ADDR_CLK2,0x20) << std::endl;
+
   printf("%06X\n",ADC.readRegister(ADS131A04_ADC::ADDR_STAT_1));
   printf("%06X\n",ADC.readRegister(ADS131A04_ADC::ADDR_STAT_P));
   printf("%06X\n",ADC.readRegister(ADS131A04_ADC::ADDR_STAT_N));
