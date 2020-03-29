09d3ee8c1765729c94daf8ee4f87711bc367c576
Christian Llanes <christianllanes96@gmail.com>
2020-03-29 17:03:30 -0400

    update

--- thrust_estimator/src/ADS131A04_test.cpp
+++ thrust_estimator/src/ADS131A04_test.cpp
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
     bcm2835_spi_begin();
     bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
     bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);                   // The default
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_64); // The default
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // The default
     bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
     bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 
@@ -50,7 +50,7 @@ int main(int argc, char **argv)
     // Can the read the reply bytes from the buffer.
     // If you tie MISO to MOSI, you should read back what was sent.
 
-    char buf[] = { 0x06, 0x55, 0x00}; // Data to send
+    char buf[] = { 0x00, 0x00, 0x00}; // Data to send
     bcm2835_spi_transfern(buf, sizeof(buf));
     // buf will now be filled with the data that was read from the slave
     printf("Read from SPI: %02X  %02X  %02X \n", buf[0], buf[1], buf[2]);
