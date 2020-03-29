f2dceeb8861c4f825ffcf74bdc168a0772541ff4
Christian Llanes <christianllanes96@gmail.com>
2020-03-29 16:33:10 -0400

    update

--- thrust_estimator/src/ADS131A04_test.cpp
+++ thrust_estimator/src/ADS131A04_test.cpp
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
     bcm2835_spi_begin();
     bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
     bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);                   // The default
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32768); // The default
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_16); // The default
     bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
     bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 
