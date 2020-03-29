2d23d4e8c3f526f2883a74bd0f280372e52059cc
Christian Llanes <christianllanes96@gmail.com>
2020-03-29 16:18:27 -0400

    update

--- thrust_estimator/src/ADS131A04_test.cpp
+++ thrust_estimator/src/ADS131A04_test.cpp
@@ -14,21 +14,50 @@
  * limitations under the License.
  */
 
-#include "ros/ros.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <linux/types.h>
-#include <linux/spi/spidev.h>
+#include <bcm2835.h>
+#include <stdio.h>
 
 void makeBuffer(char *buffer, uint16_t data);
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+    // If you call this, it will not actually access the GPIO
+// Use for testing
+//        bcm2835_set_debug(1);
 
+    if (!bcm2835_init())
+    {
+      printf("bcm2835_init failed. Are you running as root??\n");
+      return 1;
+    }
+    if (!bcm2835_spi_begin())
+    {
+      printf("bcm2835_spi_begin failed. Are you running as root??\n");
+      return 1;
+    }
+    bcm2835_spi_begin();
+    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
+    bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);                   // The default
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32768); // The default
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 
-  return 0;
+    // Send a some bytes to the slave and simultaneously read
+    // some bytes back from the slave
+    // Most SPI devices expect one or 2 bytes of command, after which they will send back
+    // some data. In such a case you will have the command bytes first in the buffer,
+    // followed by as many 0 bytes as you expect returned data bytes. After the transfer, you
+    // Can the read the reply bytes from the buffer.
+    // If you tie MISO to MOSI, you should read back what was sent.
+
+    char buf[] = { 0x00, 0x00, 0x00}; // Data to send
+    bcm2835_spi_transfern(buf, sizeof(buf));
+    // buf will now be filled with the data that was read from the slave
+    printf("Read from SPI: %02X  %02X  %02X  %02X \n", buf[0], buf[1], buf[2]);
+
+    bcm2835_spi_end();
+    bcm2835_close();
+    return 0;
 }
 
 void makeBuffer(char *buffer, uint16_t data){
