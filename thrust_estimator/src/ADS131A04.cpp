21beae1c81bf0ed9a99023f978714dadfce8b080
Christian Llanes <christianllanes96@gmail.com>
2020-07-21 01:54:26 -0400

    update

--- thrust_estimator/src/ADS131A04.cpp
+++ thrust_estimator/src/ADS131A04.cpp
@@ -383,9 +383,9 @@ uint32_t* ADS131A04::getChannels()
 
 void ADS131A04::makeBuffer_(char* buffer, uint16_t data)
 {
-  memset(buffer, 0, sizeof buffer);
-  buffer[1] = data & 0xFF;
-  buffer[0] = data >> 8;
+  memset(buffer, 0, sizeof (*buffer));
+  (*buffer)[1] = data & 0xFF;
+  (*buffer)[0] = data >> 8;
 }
 
 } /*namespace ADS131A04_ADC*/
