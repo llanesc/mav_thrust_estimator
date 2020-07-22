5e7a323f804a7cb54d7034caf5fc8015025b8934
Christian Llanes <christianllanes96@gmail.com>
2020-07-22 19:33:56 -0400

    update

--- thrust_estimator/src/ADS131A04.cpp
+++ thrust_estimator/src/ADS131A04.cpp
@@ -39,25 +39,33 @@ ADS131A04::ADS131A04(){
     ROS_ERROR("spi_init error.");
   }
 
+  sleep(1);
+
   if (gpio_export(gpioPin) < 0)
   {
     ROS_ERROR("gpio_export error.");
   }
+
+  sleep(1);
 
   if (gpio_set_direction(gpioPin,0) < 0)
   {
     ROS_ERROR("gpio_set_direction error.");
   }
 
+  sleep(1);
+
   if (gpio_set_edge(gpioPin,"falling") < 0)
   {
-    ROS_ERROR("gpio_set_direction error.");
+    ROS_ERROR("gpio_set_edge error.");
   }
+  sleep(1);
 
   if ((gpiofd = gpio_pin_open(gpioPin)) < 0)
   {
     ROS_ERROR("gpio_pin_open error.");
   }
+
   pfd.fd = gpiofd;
   pfd.events = POLLPRI | POLLERR;
 //  if ((gpiofd = gpio_init(24)) < 0)
